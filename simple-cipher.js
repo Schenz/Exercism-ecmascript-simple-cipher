83bfb36ec5b86877ee11ac049599fbb579f5b3d0
Schenz <bschenz@gmail.com>
2018-08-12 12:36:04 -0400

    final solution

--- simple-cipher.js
+++ simple-cipher.js
@@ -1,21 +1,49 @@
 class Cipher {
-    constructor() {
-        this.key = this.makeKey(100);
-    }
-
-    makeKey(keySize) {
-        let text = "";
-        const possible = "abcdefghijklmnopqrstuvwxyz";
-
-        for (let index = 0; index < keySize; index += 1) {
-            text += possible.charAt(Math.floor(Math.random() * possible.length));
+    constructor(key) {
+        if (!key && key !== '') {
+            this.key = makeKey(100);
+        } else if (hasUpperCase(key) || hasNumber(key) || key === '') {
+            throw new Error('Bad key');
+        } else {
+            this.key = key;
         }
 
-        return text;
+        function hasNumber(myString) {
+            return /\d/.test(myString);
+        };
+
+        function hasUpperCase(str) {
+            return str.toLowerCase() != str;
+        };
+
+        function makeKey(keySize) {
+            let text = "";
+            const possible = "abcdefghijklmnopqrstuvwxyz";
+
+            for (let index = 0; index < keySize; index += 1) {
+                text += possible.charAt(Math.floor(Math.random() * possible.length));
+            }
+
+            return text;
+        }
     }
 
-    encode() {
+    encode(text) {
+        let self, i;
 
+        self = this;
+        i = 0;
+
+        return text.replace(/[^a-z]/g, '').replace(/[a-z]/g, letter => String.fromCharCode((((letter.charCodeAt(0) - 97) + ((self.key[i++ % self.key.length].charCodeAt(0) - 97))) % 26 + 97)));
+    }
+
+    decode(text) {
+        let self, i;
+
+        self = this;
+        i = 0;
+
+        return text.replace(/[^a-z]/g, '').replace(/[a-z]/g, letter => String.fromCharCode((((letter.charCodeAt(0) - 97) + (26 - (self.key[i++ % self.key.length].charCodeAt(0) - 97))) % 26 + 97)));
     }
 }
 
